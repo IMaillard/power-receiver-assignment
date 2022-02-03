a91ecd7098652d4d08f0f103217f409097e302f5
gabri.ma8449 <gabri.ma8449@vusd.us>
2022-02-03 15:15:57 -0800

    Initial commit

---
+++ app/src/main/java/com/example/powerreceiver/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.powerreceiver;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CustomReceiver mReceiver = new CustomReceiver();
+    private static final String ACTION_CUSTOM_BROADCAST = BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        filter.addAction(Intent.ACTION_BATTERY_LOW);
+
+        this.registerReceiver(mReceiver, filter);
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(mReceiver,
+                        new IntentFilter(ACTION_CUSTOM_BROADCAST));
+
+    }
+
+    public void sendCustomBroadcast(View view) {
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        LocalBroadcastManager.getInstance(this)
+                .unregisterReceiver(mReceiver);
+        this.unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+
+}
